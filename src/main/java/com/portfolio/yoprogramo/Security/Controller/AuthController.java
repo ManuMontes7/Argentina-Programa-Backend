bb5bd1015bd5bffc1b329cc2817672ee468c8dbc
ManuMontes7 <99566175+ManuMontes7@users.noreply.github.com>
2022-08-27 08:52:28 -0300

    Deploy

--- src/main/java/com/portfolio/yoprogramo/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/yoprogramo/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins= "https://portfolioargprograma-9af25.web.app")
 public class AuthController {
 
     @Autowired
