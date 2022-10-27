bcbb08f2476fc1925457ec379220d38dc4b0a1c6
ManuMontes7 <99566175+ManuMontes7@users.noreply.github.com>
2022-10-27 15:17:52 -0300

    Update data.sql

--- src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO rol VALUES (NULL, 'ROLE_ADMIN'),
-                       (NULL, 'ROLE_USER');
-
-
-INSERT INTO Usuario VALUES ('admin', 'admin', 'admin@admin.com', 'MMadmin', ["admin"]),
-                           ('user', 'user', 'user@user.com', 'MMuser', ["user"]);
-
-INSERT INTO Persona VALUES (1, 'Nombre', 'Apellido', 'Descripcion Vacia', 'https://sistemas.com/wp-content/uploads/2013/12/twitpic.png');
